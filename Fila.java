913eb557b1679f183fee02a67fd9c300940b0fda
ThiagoKwon <thiago.v.s.kwon@gmail.com>
2024-10-03 20:22:02 -0300

    commit
    commit

--- Fila.java
+++ Fila.java
@@ -2,86 +2,62 @@ package com.fill.flood;
 
 public class Fila<T> {
 
-    private int top = 0; 
-    private int base = 0; 
-    private int size = 0; 
-    private T[] data; 
+	private int top = 0;
+	private int base = 0;
+	private int count = 0;
+	private T[] data;
 
-    @SuppressWarnings("unchecked")
-    public Fila(int size) {
-        setData((T[]) new Object[size]);
-        this.setSize(size);
-    }
+	@SuppressWarnings("unchecked")
+	public Fila(int size) {
+		setData((T[]) new Object[size]);
+	}
 
-    public void add(T data) {
-        if (isFull()) {
-            throw new IllegalAccessError("Fila cheia!");
-        }
-        getData()[getTop()] = data;
-        setTop(getTop() + 1);
-    }
+	public void add(T data) {
+		if (isFull()) {
+			throw new IllegalAccessError("Fila cheia!");
+		}
+		getData()[top] = data;
+		top = (top + 1) % getData().length;
+		count++;
+	}
 
-    public T remove() {
-        if (isEmpty()) {
-            throw new IllegalAccessError("Fila já está vazia!");
-        }
-        T retorno = getData()[getBase()];
-        getData()[getBase()] = null;
-        setBase(getBase() + 1);
-        return retorno;
-    }
+	public T remove() {
+		if (isEmpty()) {
+			throw new IllegalAccessError("Fila já está vazia!");
+		}
+		T retorno = getData()[base];
+		getData()[base] = null;
+		base = (base + 1) % getData().length;
+		count--;
+		return retorno;
+	}
 
-    public void clear() {
-        for (int i = 0; i < getData().length; i++) {
-            getData()[i] = null;
-        }
-        setTop(0);
-        setBase(0);
-    }
+	public boolean isFull() {
+		return count == getData().length;
+	}
 
-    public boolean isFull() {
-        return this.data.length == this.top;
-    }
+	public boolean isEmpty() {
+		return count == 0;
+	}
 
-    public boolean isEmpty() {
-        return this.top == this.base;
-    }
+	public void clear() {
+		for (int i = 0; i < getData().length; i++) {
+			getData()[i] = null;
+		}
+		top = 0;
+		base = 0;
+		count = 0;
+	}
 
-    public void exibirFila(T[] data) {
-        for (int i = 0; i < data.length; i++) {
-            System.out.println(data[i]);
-        }
-    }
+	public int getCount() {
+		return count;
+	}
 
-    public int getTop() {
-        return top;
-    }
+	public T[] getData() {
+		return data;
+	}
 
-    public void setTop(int top) {
-        this.top = top;
-    }
-
-    public int getBase() {
-        return base;
-    }
-
-    public void setBase(int base) {
-        this.base = base;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public T[] getData() {
-        return data;
-    }
-
-    public void setData(T[] data) {
-        this.data = data;
-    }
+	public void setData(T[] data) {
+		this.data = data;
+	}
 }
